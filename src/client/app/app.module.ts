d9b280075eb792d5e5b94bbeca2b1a3ec86decb7
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-02-11 15:37:38 +0100

    Added nodeExternals module

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
 
@@ -16,16 +16,13 @@ const config: SocketIoConfig = { url: 'http://localhost:5400', options: {} };
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'nestJS' }),
     HttpClientModule,
     SocketIoModule.forRoot(config),
-    RouterModule.forRoot(routes, {
-      useHash: false,
-      preloadingStrategy: PreloadAllModules,
-    })
+    RouterModule.forRoot(routes)
   ],
   providers: [
     PingService
--- src/client/app/app.server.module.ts
+++ src/client/app/app.server.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
@@ -9,8 +9,10 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule,
     ModuleMapLoaderModule
   ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
